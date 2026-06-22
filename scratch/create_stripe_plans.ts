63f7da8da8f1703afe34cdef41e5fa9b51085967
Gabe <gkouprine@gmail.com>
2026-06-22 22:20:38 +1000

    Update create_stripe_plans.ts

--- scratch/create_stripe_plans.ts
+++ scratch/create_stripe_plans.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env.local") });
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-04-10", // use whatever recent version
+  apiVersion: "2026-05-27.dahlia", // use whatever recent version
 });
 
 async function main() {
